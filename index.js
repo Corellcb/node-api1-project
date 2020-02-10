5dc68e48cf6c403a030a7385398ad77f9b7dba45
CorellCB <corellcb@gmail.com>
2020-02-10 16:26:10 -0500

    added status codes to get id request

--- index.js
+++ index.js
@@ -33,19 +33,26 @@ server.get('/api/users', (req, res) => {
         })
         .catch(err => {
             console.log(err);
-            res.status(500).json({ errorMessage: 'oops' });
+            res.status(500).json({ errorMessage: 'The user information could not be retrieved.' });
         })
 })
 
 server.get('/api/users/:id', (req, res) => {
     const {id} = req.params;
 
-    db.findById(id).then(db => {
-        res.status(200).json(db)
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json({ errorMessage: 'oops' });
-    })
+    db
+        .findById(id)
+        .then(db => {
+           if(db === undefined){
+               res.status(404).json({ errorMessage: 'The user with the specified ID does not exist.'})
+           } else {
+               res.status(200).json(db)
+           }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ errorMessage: 'The user information could not be retrieved.' });
+        })
 })
 
 const port = 5000;
